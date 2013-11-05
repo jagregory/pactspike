9ad64877f3f486ea13cc80f67ec7c2b3fcfc359d
James Gregory <james@jagregory.com>
2013-11-06 10:05:34 +1100

    Test using Rack Reverse Proxy to forward requests onto another non-ruby server

--- spec/pact_helper.rb
+++ spec/pact_helper.rb
@@ -9,6 +9,19 @@ Pact.provider_states_for 'First Name Getter' do
   end
 end
 
+# using actual Rack app instance
 Pact.service_provider "Name service" do
   app { Sinatra::Application.new }
 end
+
+# using reverse proxy to another server
+# require 'rack/reverse_proxy'
+# class Proxy < Sinatra::Base
+#   use Rack::ReverseProxy do 
+#     reverse_proxy '/', 'http://localhost:9292/'
+#   end
+# end
+
+# Pact.service_provider "Name service" do
+#   app { Proxy.new }
+# end
